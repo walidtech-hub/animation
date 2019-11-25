592b6b57647e5adb58e55ad039be8ad71bceaa06
walidtech-hub <57053387+walidtech-hub@users.noreply.github.com>
2019-11-24 20:12:21 -0600

    Add files via upload

---
+++ AnimationHM.java
@@ -0,0 +1,196 @@
+/*
+
+*/
+import java.lang.*;
+import java.io.*;
+
+public class AnimationHM{
+
+
+
+  public static void main(String[]args) throws IOException, InterruptedException
+  {
+    //create sprites as string arrays.
+
+
+      String[] spriteHM1 =
+      {
+"                                                     ",
+"                                                     ",
+"                                                     ",
+"                                                     ",
+"                     ||                              ",
+"                     ||                              ",
+"                     ||                              ",
+"                     ||                              ",
+"                     ||                              ",
+"                     ||                              ",
+"                     ||                              ",
+"                     ||                              ",
+"                     ||                              ",
+"              ||=============||                      ",
+"              ||=============||                      ",
+"              ||=============||                      ",
+"--------------||=============||----------------------",
+		};
+
+
+	  String[] spriteHM2 =
+      {
+
+"                      ___________________              ",
+"                     ||                                ",
+"                     ||                                ",
+"                     ||                                ",
+"                     ||                                ",
+"                     ||                                ",
+"                     ||                                ",
+"                     ||                                ",
+"                     ||                                ",
+"                     ||                                ",
+"              ||=============||                        ",
+"              ||=============||                        ",
+"              ||=============||                        ",
+"--------------||=============||------------------------",
+		};
+    String[] spriteHM3 =
+      {
+
+"                      ___________________                  ",
+"                     ||                 ||                 ",
+"                     ||                 ||                 ",
+"                     ||                 ||                 ",
+"                     ||                 ||                 ",
+"                     ||                 ||                 ",
+"                     ||                                    ",
+"                     ||                                    ",
+"                     ||                                    ",
+"                     ||                                    ",
+"              ||=============||                            ",
+"              ||=============||                            ",
+"              ||=============||                            ",
+"--------------||=============||-----------------------------",
+    };
+
+String[] spriteHM4 ={
+
+  "                                                             ",
+  "                                           { ^ ^ }           ",
+  "                                             ***             ",
+  "                                              |              ",
+  "                                      []======|======[]      ",
+  "                                              |              ",
+  "                                              |              ",
+  "                                              |              ",
+  "                                           =======          ",
+  "                                          ||     ||         ",
+  "                                          ||     ||         ",
+  "                                          ||     ||         ",
+  "                                       ---------------      ",
+};
+
+
+
+
+	  String[] spriteHM5 =
+      {
+"                      ________________________             ",
+"                      ||                     ||             ",
+"                      ||                     ||             ",
+"                      ||                     ||             ",
+"                      ||                     ||             ",
+"                      ||                     ||             ",
+"                      ||                     ||             ",
+"                      ||                   { ^ ^ }           ",
+"                      ||                     ***             ",
+"                      ||                      |              ",
+"                      ||                ======|======        ",
+"                      ||                      |              ",
+"                      ||                      |              ",
+"                      ||                      |              ",
+"                      ||                   =======           ",
+"                ||=============||         ||     ||          ",
+"                ||=============||         ||     ||          ",
+"                ||=============||                            ",
+"----------------||=============||-----------------------------",
+
+    };
+    String[] spriteHM6 =
+      {
+        "What do you think is happed after the winter has been hangged ? ",
+      };
+      String[] spriteHM7 =
+        {
+          "Let's find out! ?",
+        };
+
+
+    print(spriteHM1);
+		Thread.sleep(1500);
+		print(spriteHM2);
+		Thread.sleep(1500);
+		print(spriteHM3);
+    Thread.sleep(1500);
+		animate(spriteHM4);
+    Thread.sleep(1500);
+		print(spriteHM5);
+    Thread.sleep(1500);
+    print(spriteHM6);
+    Thread.sleep(5500);
+    print(spriteHM7);
+    Thread.sleep(5500);
+
+
+  }// end main
+
+  /**
+  The method print is used to print one element of a string array per line.
+  It can be used for text or ascii art.
+  @param String[] printable
+  */
+   public static void print(String[] printable) throws IOException, InterruptedException
+   {
+     cls();
+     //System.out.println(ANSI_CLEAR_SCREEN + ANSI_RED + ANSI_BOLD);
+     for(int k = 0; k < printable.length; k++)
+     {
+		System.out.println(printable[k]);
+     }
+	 //System.out.println(ANSI_RESET);
+   }
+/**
+   The method Animate is used to print one element of a string array per line.
+   with the addition of space before printing the array. The space is reduced by
+   one each time and then printing continues.
+   It can be used for text or ascii art.
+   @param String[] printable
+   */
+   public static void animate(String[] printable) throws IOException, InterruptedException
+   {
+     cls();
+     //System.out.println(ANSI_CLEAR_SCREEN + ANSI_GREEN + ANSI_BOLD);
+
+     for(int path = 0; path < printable.length; path++)
+     {
+	   Thread.sleep(500);
+     cls();
+     //System.out.println(ANSI_CLEAR_SCREEN + ANSI_GREEN + ANSI_BOLD);
+       for(int k = 0; k < printable.length; k++)
+       {
+         for(int space = printable.length; space > path; space--)
+         {
+          System.out.print(" ");
+         }
+		  System.out.println(printable[k]);
+       }
+     }
+     	 //System.out.println(ANSI_RESET);
+   }// end animate
+
+/**
+ * a quick process that clears the screen  Currently only for windows users.
+ */
+    public static void cls() throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    }//end cls
+}//end class
